3253c74a8d64c1a51e4df41954c32c27befc19c1
Phuri-Thothong <phuri.thothong@gmail.com>
2026-05-06 13:53:00 +0700

    feat(console):  เพิ่มคำสั่ง trash:clear และระบบล้างถังขยะอัตโนมัติสำหรับข้อมูลที่ค้างเกิน 30 วัน

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Models\Service;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('trash:clear', function(){
+    Service::onlyTrashed()->forceDelete();
+    $this->info('ถังขยะถูกทิ้งเรียบร้อยแล้ว');
+})->purpose('เพื่อทิ้งข้อมูลที่อยู่ในถังขยะทั้งหมด');
+
+Schedule::call(function(){
+    Service::onlyTrashed()
+        ->where('deleted_at', '<', now()->subDays(30))
+        ->forceDelete();
+})->daily();
