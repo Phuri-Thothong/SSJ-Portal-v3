5a6015b44c5d9c2366debd1fca858164ea64a852
Phuri-Thothong <phuri.thothong@gmail.com>
2026-05-18 09:01:40 +0700

    feat(navbar): เพิ่มปุ่มออกจากระบบ และแสดงข้อมูลชื่อพร้อมกลุ่มงานจริงของบุคลากร

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FontSizeService } from '../../services/font-size.service';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -11,4 +13,17 @@ import { FontSizeService } from '../../services/font-size.service';
 })
 export class NavbarComponent {
   fontSizeService = inject(FontSizeService);
+  public authService = inject(AuthService);
+  public router = inject(Router);
+
+  onLogout() {
+    this.authService.logout().subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+      },
+      error: () => {
+        this.router.navigate(['/login']);
+      }
+    })
+  }
 }
